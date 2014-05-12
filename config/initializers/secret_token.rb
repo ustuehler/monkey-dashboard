0d124fa97dd8342e4b396b86deec6ad61937fb2e
Uwe Stuehler <uwe@bsdx.de>
2014-05-12 02:04:35 +0200

    Initial commit with bootstrap-sass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonkeyDashboard::Application.config.secret_key_base = '01aeb8ee7a55d84a645bb851def3f66c0efc513c37aae2bcba56fe4da47129bf3856c41e286a0a11601e628192da5a473906723c99823fe3441611a5a11d2ad1'
