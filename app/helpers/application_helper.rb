aa990a90b03b3fefaa5c9f528a389b2b0b0b86dc
Uwe Stuehler <uwe@bsdx.de>
2014-05-24 15:24:44 +0200

    Replace the home view with a markdown template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Bootstrap CSS contextual colors
+  [:muted, :primary, :success, :info, :warning, :danger].each do |level|
+    define_method(level) do |text|
+      content_tag :span, text, :class => "text-#{level}"
+    end
+  end
+
+  def human_date(date = Date.today)
+    date.strftime '%A, %B %-d'
+  end
 end
---
+++ app/views/welcome/home.md
@@ -0,0 +1,4 @@
+Welcome home!
+=============
+
+Today is <%= info human_date %> and your overall financial situation is <%= danger 'unknown' %>.
