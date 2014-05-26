9d1ccae3386ff3c0dc27469e8d04b5ac1d564b8e
Uwe Stuehler <uwe@bsdx.de>
2014-05-26 21:30:08 +0200

    JS-based submenu in sidebar menu

--- app/assets/javascripts/bootstrap_config.js
+++ app/assets/javascripts/bootstrap_config.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $('button[data-toggle="offcanvas"]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
+
+  $('a.tree-toggle').click(function () {
+    $(this).parent().children('ul.tree').slideToggle();
+  });
+
+  $('a.tree-toggle').removeAttr('href');
 });
